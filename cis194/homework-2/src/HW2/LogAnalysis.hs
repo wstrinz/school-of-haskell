5ee29adb926d0494f801c7f53a9dfb9a7d7d3aa5
Will Strinz <wstrinz@gmail.com>
2015-03-27 16:24:57 -0500

    done!

--- cis194/homework-2/src/HW2/LogAnalysis.hs
+++ cis194/homework-2/src/HW2/LogAnalysis.hs
@@ -8,21 +8,62 @@ module HW2.LogAnalysis
 ) where
 
 import Provided.Log
+import Data.List (foldl')
+
+mcode :: String -> Int
+mcode msg = read code
+  where (code:_) = words msg
+
+msgpart :: String -> String
+msgpart msg = unwords rest
+  where (_:rest) = words msg
 
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage ('E':' ':xs) = LogMessage (Error num) priority msg
+  where (num_s:priority_s:msg_ws) = words xs
+        num = read num_s
+        priority = read priority_s
+        msg = unwords msg_ws
+
+parseMessage ('I':' ':xs) = LogMessage Info (mcode xs) (msgpart xs)
+parseMessage ('W':' ':xs) = LogMessage Warning (mcode xs) (msgpart xs)
+parseMessage _ = Unknown "This is not in the right format"
 
 parse :: String -> [LogMessage]
-parse = undefined
+parse = map parseMessage . lines
+
+timestamp :: LogMessage -> Maybe TimeStamp
+timestamp (LogMessage _ stamp _) = Just stamp
+timestamp _ = Nothing
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg (Node left rootNodeMessage right)
+  | msgTime < nodeTime = Node (insert msg left) rootNodeMessage right
+  | msgTime > nodeTime = Node left rootNodeMessage (insert msg right)
+  | msgTime == nodeTime = Node left msg right
+  where msgTime = timestamp msg
+        nodeTime = timestamp rootNodeMessage
+insert _ _ = Leaf
+-- insert msg _ = Node Leaf msg Leaf
+
+-- Node Leaf (LogMessage (Error 9000) 2 "") Leaf
 
 build :: [LogMessage] -> MessageTree
-build = undefined
+build = foldr insert Leaf
+-- build = foldl' (flip insert) Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node Leaf msg Leaf) = [msg]
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong msgs = map msgstring $ filter isError $ inOrder $ build msgs
+  where isError (LogMessage (Error severity) _ _)
+          | severity > 49 = True
+          | otherwise = False
+        isError _ = False
+        msgstring (LogMessage _ _ str) = str
+        msgstring (Unknown str) = str
