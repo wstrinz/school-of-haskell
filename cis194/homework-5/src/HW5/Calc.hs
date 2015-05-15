9fe9e52991825406103e1702b12d68dff3a76748
Will Strinz <wstrinz@gmail.com>
2015-05-15 17:00:41 -0500

    finish hw5

--- cis194/homework-5/src/HW5/Calc.hs
+++ cis194/homework-5/src/HW5/Calc.hs
@@ -18,6 +18,7 @@ module HW5.Calc
 ) where
 
 import Provided.Parser (parseExp)
+import Control.Applicative
 
 import qualified Data.Map         as Map
 import qualified Provided.ExprT   as ExprT
@@ -26,12 +27,14 @@ import qualified Provided.StackVM as StackVM
 -- exercise 1
 
 eval :: ExprT.ExprT -> Integer
-eval = undefined
+eval (ExprT.Lit val) = val
+eval (ExprT.Mul exp1 exp2) = (eval exp1) * (eval exp2)
+eval (ExprT.Add exp1 exp2) = (eval exp1) + (eval exp2)
 
 -- exercise 2
 
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr = parseExp id (+) (*)
 
 -- exercise 3
 
@@ -41,9 +44,9 @@ class Expr a where
   mul :: a -> a -> a
 
 instance Expr ExprT.ExprT where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = ExprT.Lit
+  add = ExprT.Add
+  mul = ExprT.Mul
 
 -- exercise 4
 
@@ -51,34 +54,35 @@ newtype MinMax = MinMax Integer deriving (Eq, Show)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
 
 instance Expr Integer where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = id
+  add = (+)
+  mul = (*)
 
 instance Expr Bool where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = (>= 0)
+  add = (&&)
+  mul = (||)
 
 instance Expr MinMax where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = (MinMax)
+  add (MinMax x) (MinMax y) = MinMax $ maximum [x, y]
+  mul (MinMax x) (MinMax y) = MinMax $ minimum [x, y]
 
 instance Expr Mod7 where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = (Mod7)
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
 
 -- exercise 5 (optional)
 
 instance Expr StackVM.Program where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit x = [StackVM.PushI x]
+  -- add p1 p2 = p1 ++ p2 ++ [StackVM.Add]
+  add = (. (++ [StackVM.Add])) . (++)
+  mul p1 p2 = p1 ++ p2 ++ [StackVM.Mul]
 
 compile :: String -> Maybe StackVM.Program
-compile = undefined
+compile = parseExp lit add mul
 
 -- exercise 6 (optional)
 
@@ -90,20 +94,29 @@ data (Eq a, Show a) => VarExprT a
   deriving (Eq, Show)
 
 instance (Eq a, Show a) => Expr (VarExprT a) where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit = Lit
+  add = Add
+  mul = Mul
 
 class HasVars a where
   var :: String -> a
 
 instance HasVars (VarExprT String) where
-  var = undefined
+  var = Var
 
 instance HasVars (Map.Map String Integer -> Maybe Integer) where
-  var = undefined
+  var = (Map.lookup)
+
+-- maybeAdd :: Maybe Integer -> Maybe Integer -> Maybe Integer
+-- maybeAdd x y = (<*> (+))
 
 instance Expr (Map.Map String Integer -> Maybe Integer) where
-  lit = undefined
-  add = undefined
-  mul = undefined
+  lit x _ = Just x
+  -- add mf1 mf2 aMap = case (mf1 aMap) of
+  --   Nothing -> Nothing
+  --   Just x -> case (mf2 aMap) of
+  --     Nothing -> Nothing
+  --     Just y -> Just (x + y)
+  --add mf1 mf2 m = (+) <$> mf1 m <*> mf2 m
+  add mf1 mf2 m = Just (+) <*> mf1 m <*> mf2 m
+  mul = mul
